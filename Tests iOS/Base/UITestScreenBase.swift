e1b94d589467302afe31a8e937f4827b1067079f
3472204455ilia <68978403+3472204455ilia@users.noreply.github.com>
2022-04-12 00:03:13 -0400

    base

---
+++ Tests iOS/Base/UITestScreenBase.swift
@@ -0,0 +1,20 @@
+//
+//  UITestBase.swift
+//  Tests iOS
+//
+//  Created by Pavlov, Ilia on 4/1/22.
+//
+
+import Foundation
+import XCTest
+
+
+class UITestScreenBase {
+    var app: XCUIApplication
+    var testCase: XCTestCase
+    
+    init(app: XCUIApplication, testCase: XCTestCase) {
+        self.app = app
+        self.testCase = testCase
+    }
+}
